5ee2c5db5be4d150af32209a47d815e1cd9ecf9d
omarjab01 <omar241101skate@gmail.com>
2022-08-19 21:51:05 +0200

    note taking app using localstorate to store notes

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -23,7 +23,7 @@ const Main = ({activeNote, editNote}) => {
             onChange={(event) => editField("title",event.target.value)}/>
             <textarea placeholder='Scrivi i tuoi appunti' className="p-4 h-screen rounded-xl" value={activeNote.body} onChange={(event) => editField("body",event.target.value)}/>
         </div>
-        <div className='col-span-3 border-l p-4'>
+        <div className='col-span-3 border-l p-4 h-screen overflow-auto mb-2'>
             <h2 className='text-xl font-semibold'>{activeNote.title}</h2>
             <ReactMarkdown className='mt-6'>{activeNote.body}</ReactMarkdown>
         </div>
